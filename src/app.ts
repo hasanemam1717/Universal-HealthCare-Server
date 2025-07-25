6c666a4bbeeddf266d5e9fa09fac0883f3d7a66e
hasanemam1717 <hasanemam1717@gmail.com>
2025-07-25 22:09:56 +0600

    Managing Not Found Routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors'
 import router from './app/modules/routes';
+import status from 'http-status';
 const app: Application = express()
 app.use(cors())
 
@@ -15,6 +16,17 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.use('/api/v1/', router)
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
+    res.status(status.NOT_FOUND).json({
+        success: false,
+        massage: "API not found",
+        error: {
+            path: req?.originalUrl,
+            errorMassage: "Your requested path is not found."
+        }
+    })
+})
 
 
 
