dfd2e290063198755b5ee0d6bd3d908a3d5ffbc2
hasanemam1717 <hasanemam1717@gmail.com>
2025-07-29 11:12:45 +0600

    Implementing Request Validation Middleware

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,94 +1,80 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { adminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
 
 import status from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 
 
 
-const getAllAdminData = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const filters = pick(req.query, adminFilterableFields)
-        const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder'])
-        const result = await adminService.getAllAdminData(filters, options)
 
-        sendResponse(res, {
-            statusCode: status.OK,
-            massage: "Admin data get successfully",
-            meta: result.meta,
-            data: result.data
-        })
 
-    } catch (err: any) {
-        next(err)
-    }
-}
-const getDataById = async (req: Request, res: Response, next: NextFunction) => {
+const getAllAdminData = catchAsync(async (req: Request, res: Response) => {
+
+    const filters = pick(req.query, adminFilterableFields)
+    const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder'])
+    const result = await adminService.getAllAdminData(filters, options)
+
+    sendResponse(res, {
+        statusCode: status.OK,
+        massage: "Admin data get successfully",
+        meta: result.meta,
+        data: result.data
+    })
+
+
+})
+const getDataById = catchAsync(async (req: Request, res: Response) => {
     const { id } = req.params
-    console.log(id);
-    try {
-        const result = await adminService.getDataById(id)
-        sendResponse(res, {
-            statusCode: status.OK,
-            success: true,
-            massage: "Admin get by id",
-            data: result
-        })
+    // console.log(id);
+    const result = await adminService.getDataById(id)
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        massage: "Admin get by id",
+        data: result
+    })
 
-    } catch (err: any) {
-        next(err)
-    }
 
-}
-const updateIntoDb = async (req: Request, res: Response, next: NextFunction) => {
+
+})
+const updateIntoDb = catchAsync(async (req: Request, res: Response) => {
     const { id } = req.params
-    try {
-        const result = await adminService.updateIntoDb(id, req.body)
-        sendResponse(res, {
-            statusCode: status.OK,
-            success: true,
-            massage: "Admin updated by id",
-            data: result
-        })
-
-    } catch (err: any) {
-        next(err)
-    }
-}
+    const result = await adminService.updateIntoDb(id, req.body)
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        massage: "Admin updated by id",
+        data: result
+    })
+})
 
 
-const deleteFromDb = async (req: Request, res: Response, next: NextFunction) => {
+const deleteFromDb = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
-    try {
-        const result = await adminService.deleteFromDb(id)
-        sendResponse(res, {
-            statusCode: status.OK,
-            success: true,
-            massage: "Admin delete by id",
-            data: result
-        })
+    const result = await adminService.deleteFromDb(id)
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        massage: "Admin delete by id",
+        data: result
+    })
 
-    } catch (err: any) {
-        next(err)
-    }
-}
-const softDeleteFromDb = async (req: Request, res: Response, next: NextFunction) => {
+})
+const softDeleteFromDb = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
-    try {
-        const result = await adminService.softDeleteFromDb(id)
-        sendResponse(res, {
-            statusCode: status.OK,
-            success: true,
-            massage: "Admin get by id",
-            data: result
-        })
+    const result = await adminService.softDeleteFromDb(id)
+    sendResponse(res, {
+        statusCode: status.OK,
+        success: true,
+        massage: "Admin get by id",
+        data: result
+    })
 
-    } catch (err: any) {
-        next(err)
-    }
-}
+
+})
 export const adminController = {
     getAllAdminData,
     getDataById,
--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -1,11 +1,18 @@
-import express from "express";
+import { AnyZodObject } from './../../../../node_modules/zod/v3/types.d';
+
+import express, { NextFunction, Request, Response } from "express";
 import { adminController } from "./admin.controller";
+import { z } from "zod";
+import validateRequest from '../../middlewares/validateRequest';
+import { adminValidationSchemas } from './admin.validation';
 
 const router = express.Router()
 
+
+
 router.get("/", adminController.getAllAdminData)
 router.get('/:id', adminController.getDataById)
-router.patch('/:id', adminController.updateIntoDb)
+router.patch('/:id', validateRequest(adminValidationSchemas.update), adminController.updateIntoDb)
 router.delete('/:id', adminController.deleteFromDb)
 router.delete('/soft/:id', adminController.softDeleteFromDb)
 
