7cecd36f70ba5a0bc981b5214c75629821747fc6
hasanemam1717 <hasanemam1717@gmail.com>
2025-09-19 09:41:31 +0600

    added schedule slution

--- src/app/modules/Schedule/schedule.route.ts
+++ src/app/modules/Schedule/schedule.route.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { scheduleController } from './schedule.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '../../../generated/prisma';
 const router = express.Router()
 
-router.post('/', scheduleController.insertIntoDb)
+router.post('/', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), scheduleController.insertIntoDb)
 export const scheduleRotes = router
