c775487d1a988db9a83da3045c4a1f6386145d2e
hasanemam1717 <hasanemam1717@gmail.com>
2025-10-29 19:12:39 +0600

    Handling uncaughtException and unhandledRejection Errors

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,14 @@ async function main() {
     const server: Server = app.listen(config.port, () => {
         console.log("Sever is running on port ", config.port);
     });
-
-
+    process.on("uncaughtException", (error) => {
+        console.log(error);
+        if (server) {
+            server.close(() => {
+                console.info("Sorry, Server closed for uncaught exception!!");
+            })
+        }
+    })
 };
 
 main();
